12457ce6ff93baf33885bbbbb70eef61ac37c6d2
Mohamed <jeblister@gmail.com>
2022-02-05 08:53:02 +0100

    define content type

---
+++ contentlayer.config.ts
@@ -0,0 +1,56 @@
+import {
+  defineDocumentType,
+  ComputedFields,
+  makeSource
+} from 'contentlayer/source-files'
+import readingTime from 'reading-time';
+
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `blog/*.mdx`,
+bodyType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      description: 'The title of the post',
+      required: true,
+    },
+    date: {
+      type: 'date',
+      description: 'The date of the post',
+      required: true,
+    },
+    summary: {
+      type: 'string',
+      required: true
+    },
+    image: {
+      type: 'string',
+      required: true
+    }
+  },
+  computedFields
+}));
+
+
+const computedFields: ComputedFields = {
+  readingTime: {
+    type: 'json',
+    resolve: (doc) => readingTime(doc.body.raw)
+  },
+  wordCount: {
+    type: 'number',
+    resolve: (doc) => doc.body.raw.split(/\s+/gu).length
+  },
+
+  slug: {
+    type: 'string',
+    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, '')
+  }
+};
+
+export default makeSource({
+  contentDirPath: 'data',
+  documentTypes: [Post],
+});
