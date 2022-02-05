a01f40976822a8a8bff268d72e8a4a1eab1b80d9
Mohamed <jeblister@gmail.com>
2022-02-05 09:28:34 +0100

    delete tailwindcss classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { compareDesc, format, parseISO } from 'date-fns'
+import { format, parseISO } from 'date-fns'
 import { allPosts } from '.contentlayer/data'
 import { pick } from "@contentlayer/client";
 
@@ -19,13 +19,13 @@ export function getStaticProps() {
 
 function PostCard(post) {
   return (
-    <div className="mb-6">
-      <time dateTime={post.date} className="block text-sm text-gray-600">
+    <div>
+      <time dateTime={post.date} >
         {format(parseISO(post.date), 'LLLL d, yyyy')}
       </time>
-      <h2 className="text-lg">
+      <h2>
         <Link href={`/blog/${post.slug}`}>
-          <a className="text-blue-700 hover:text-blue-900">{post.title}</a>
+          <a>{post.title}</a>
         </Link>
       </h2>
     </div>
@@ -34,12 +34,12 @@ function PostCard(post) {
 
 export default function Home({ posts }) {
   return (
-    <div className="max-w-2xl mx-auto py-16 text-center">
+    <div>
       <Head>
         <title>Contentlayer Blog Example</title>
       </Head>
 
-      <h1 className="text-3xl font-bold mb-8">Contentlayer Blog Example</h1>
+      <h1>Contentlayer Blog Example</h1>
 
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
